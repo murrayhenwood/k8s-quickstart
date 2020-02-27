d52797ac9f0f7fc82ee9de4940321ef812d706c2
Murray Henwood <murrayhenwood@gmail.com>
2020-02-28 09:58:51 +1300

    Update Cluster master installation.sh

--- Cluster master installation.sh
+++ Cluster master installation.sh
@@ -18,15 +18,17 @@ sudo bash -c 'cat <<EOF >/etc/apt/sources.list.d/kubernetes.list
 deb https://apt.kubernetes.io/ kubernetes-xenial main
 EOF'
 
+
 #Update the package list and use apt-cache to inspect versions available in the repository
 sudo apt-get update
 
+#check what the latest version is
 apt-cache policy kubelet | head -n 20 
-
 apt-cache policy docker.io | head -n 20 
 
 #Install the required packages, if needed we can request a specific version
 sudo apt-get install -y docker.io kubelet kubeadm kubectl
+
 sudo apt-mark hold docker.io kubelet kubeadm kubectl
 
 #Check the status of our kubelet and our container runtime, docker.
