06fb505063dd3c33c041bd04f2be2869508d415a
Murray Henwood <murrayhenwood@gmail.com>
2020-02-28 11:40:47 +1300

    Update nginx-setup.sh

--- nginx/nginx-setup.sh
+++ nginx/nginx-setup.sh
@@ -1,4 +1,15 @@
-wget 
+
+
+
+wget https://raw.githubusercontent.com/murrayhenwood/k8s-quickstart/master/nginx/ns-and-sa.yaml
+wget https://raw.githubusercontent.com/murrayhenwood/k8s-quickstart/master/nginx/rbac.yaml
+wget https://raw.githubusercontent.com/murrayhenwood/k8s-quickstart/master/nginx/nginx-config.yaml
+wget https://raw.githubusercontent.com/murrayhenwood/k8s-quickstart/master/nginx/custom-resource-definitions.yaml
+wget https://raw.githubusercontent.com/murrayhenwood/k8s-quickstart/master/nginx/nginx-ingress-daemon-set.yaml
+wget https://raw.githubusercontent.com/murrayhenwood/k8s-quickstart/master/nginx/loadbalancer.yaml
+
+
+
 
 kubectl apply -f ns-and-sa.yaml
 
@@ -9,7 +20,6 @@ kubectl apply -f nginx-config.yaml
 kubectl apply -f custom-resource-definitions.yaml
 
 kubectl apply -f nginx-ingress-daemon-set.yaml
-
 
 kubectl apply -f loadbalancer.yaml
 
