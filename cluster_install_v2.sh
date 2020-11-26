96560d9a5312d6b5265099c5923611e94230b679
Murray Henwood <murrayhenwood@gmail.com>
2020-11-26 16:35:48 +1300

    Update cluster_install_v2.sh

--- cluster_install_v2.sh
+++ cluster_install_v2.sh
@@ -11,12 +11,16 @@ sudo swapoff -a
 sudo nano /etc/fstab
 
 sudo apt-get update && sudo apt-get install -y apt-transport-https curl
+
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
 cat <<EOF | sudo tee /etc/apt/sources.list.d/kubernetes.list
 deb https://apt.kubernetes.io/ kubernetes-xenial main
 EOF
+
 sudo apt-get update
+
 sudo apt-get install -y docker.io kubelet kubeadm kubectl
+
 sudo apt-mark hold docker.io kubelet kubeadm kubectl
 
 #Check the status of our kubelet and our container runtime, docker.
